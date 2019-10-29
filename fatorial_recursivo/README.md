019470a58b2e878945f5fabddebcc251df9799df
ruanpato <ruanpato@outlook.com>
2019-10-29 17:58:28 -0300

    README.md updated and *menu* to calculate other number in assembly

--- fatorial_recursivo/README.md
+++ fatorial_recursivo/README.md
@@ -1,10 +1,16 @@
 # Programação assembly RISC-V: Fatorial Recursivo #
 
-[código](https://github.com/ruanpato/gex612/tree/master/fatorial_recursivo/fatorial_recursivo.s).
+[código assembly](https://github.com/ruanpato/gex612/tree/master/fatorial_recursivo/fatorial_recursivo.s).
+[código em C](https://github.com/ruanpato/gex612/tree/master/fatorial_recursivo/fatorial_recursivo.c).
 
 Implementar a função fatorial de N através de uma função recursiva. A função recebe em a0 o valor N a ser calculado o fatorial (N!) e devolve o resultado calculado no registrador a1. Dica utilize o registrador SP(stack pointer) para salvar o conteúdo dos registradores ra e a0 na memória durante as chamadas recursivas.
 
+**Extras:**
+[Fibonacci recursivo em assembly](https://github.com/ruanpato/gex612/tree/master/fatorial_recursivo/fibonacci_recursivo.s).
+[Fibonacci recursivo em C](https://github.com/ruanpato/gex612/tree/master/fatorial_recursivo/fibonacci_recursivo.c).
+
 ## Ferramentas utilizadas ##
+
 * [RARS](https://github.com/TheThirdOne/rars) - RISC-V Assembler and Runtime Simulator
 * [GNU-GCC](https://gcc.gnu.org/) - GNU gcc c compiler
 
--- fatorial_recursivo/fatorial_recursivo.s
+++ fatorial_recursivo/fatorial_recursivo.s
@@ -4,6 +4,7 @@
     str2: .string "O valor de "
     str3: .string "! é "
     str4: .string ".\n"
+    str5: .string "1) Para calcular o fatorial de outro número.\n0) Para encerrar a execução.\n-> "
 .text
 # -------------------- Function main -------------------- #
 #                     Arguments: N/A                      #
@@ -14,31 +15,46 @@ main:
     la      a0, str1                                      # a0 receive address of str1
     li      a7, 4                                         # Enviroment Call loaded in a7 to print string
     ecall                                                 # Ecall
+
     li      a7, 5                                         # Enviroment Call to read integer
     ecall                                                 # Ecall
+
     add     s0, a0, zero                                  # Save the value Read in s0
     jal     factorial                                     # Jump to Fibonacci and link at this point in ra
+    add     s1, a1, zero                                  # Save the factorial value in s1
 
     # Printing Fatorial result:
     la      a0, str2                                      # a0 receive address of str2
     li      a7, 4                                         # Enviroment Call loaded in a7 to print string
     ecall                                                 # Ecall
+
     # a1 is the int
     li      a7, 1                                         # Enviroment Call loaded in a7 to print integer
     add     a0, s0, zero                                  # s0 is the value who is solicited factorial
     ecall                                                 # Ecall
+
     la      a0, str3                                      # a0 receive address of str3
     li      a7, 4                                         # Enviroment Call loaded in a7 to print string
     ecall                                                 # Ecall
+
     li      a7, 1                                         # Enviroment Call loaded in a7 to print integer
-    add     a0, a1, zero                                  # a1 is the calculated s0 factorial
+    add     a0, s1, zero                                  # a1 is the calculated s0 factorial
     ecall                                                 # Ecall
+
     la      a0, str4                                      # a0 receive address of str4
     li      a7, 4                                         # Enviroment Call loaded in a7 to print string
     ecall                                                 # Ecall
 
-    j       exit                                          # Finish the program execution
+    la      a0, str5                                      # a0 receive address of str5
+    li      a7, 4                                         # Enviroment Call loaded in a7 to print string
+    ecall                                                 # Ecall
 
+    li      a7, 5                                         # Enviroment Call to read integer
+    ecall                                                 # Ecall
+
+    bne     a0, zero, main                                # Goes to begin of code
+
+    j       exit                                          # Finish the program execution
 
 # ----------------- Function factorial ------------------ #
 #                     Arguments: a0                       #
