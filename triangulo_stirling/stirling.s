f174e85dcbc01892c0b44312e269bd0628fec403
ruanpato <ruanpato@outlook.com>
2019-10-30 16:26:34 -0300

    Have some error to fix in ST_2

--- triangulo_stirling/stirling.s
+++ triangulo_stirling/stirling.s
@@ -19,10 +19,29 @@ main:
 
     li      a7, 5                                         # 4 é a "Enviroment Call" que faz "leitura" de "inteiro"
     ecall                                                 # Realiza ecall
+    add     s0, a0, zero
+    add     s1, a0, zero
+    jal     until_k
+    #add     t6, a0, zero  # t6 = n
+    
+    #li      a7, 5
+    #ecall
 
-    mv      s0, a0                                        #
-    mv      s2, a0                                        # 
-    jal     until_k                                       #
+    #add     a1, a0, zero    # a1 = k
+    #add     a0, t6, zero    # a0 = t6
+    #jal     ST_2
+
+    #add     s0, a0, zero                                    # n
+    #add     s1, a0, zero                                    # k
+    #jal     until_k                                         #
+
+    mv      a0, a2
+    li      a7, 1
+    ecall
+    la      a0, str2                                  # Carrega o endereço de str0 em a0
+    li      a7, 4                                     # 4 é a "Enviroment Call" que faz "impressão" de "string"
+    ecall                                             # Realiza ecall
+
 
     j       exit                                          # Vai para exit
 
@@ -37,58 +56,105 @@ exit:
 # ------------------- Function: ST_2 -------------------- #
 #                 Arguments: a0(n), a1(k)                 #
 #           Registers in use:             #
-#                      Returns: N/A                       #
+#                      Returns: a2                        #
 #                     Comments: N/A                       #
+# int ST_2(int n, int k){
+##    if(k==0)
+#        return 0;
+##    if(n > 1)
+#        return k * ST_2(n-1, k) + ST_2(n-1, k-1);
+##    if(k > 1)
+#        return 0;
+#    return 1;
+#}
 ST_2:
-    addi    sp, sp, -16                                   # Aloca espaço na pilha
-    sw      ra, 0(sp)                                     # Aloca endereço de retorno na primeira posição da pilha
-    sw      s3, 4(sp)                                     # Aloca o 'resultado' na pilha
-    bne     a1, zero, ST_2_recursion                      # S2 k != 0
-    addi    a1, zero, 1                                   # a1 recebe 1
+    ## if(n<0)
+    slt     t4, a0, zero
+    bne     t4, zero, ST_2_k_0
+    ## if(k<0)
+    slt     t4, a1, zero
+    bne     t4, zero, ST_2_k_0
+    ## if(n==k)
+    beq     a0, a1, ST_2_k_1                              # if (n==k)
+    ## if(k==0)
+    beq     a1, zero, ST_2_k_0                            # if(k==0)
+    ## if(n>1)
+    li      t4, 1                                         # t4 = 1
+    slt     t5, t4, a0                                    # if(1 < n) t5 = 1 else t5 = 0
+    bne     t5, zero, ST_2_recursion                      # if(n > 1) return k * ST_2(n-1, k)+ST_2(n-1, k-1)
+    ## if(k>1)
+    li      t4, 1                                         # t4 = 1
+    slt     t5, t4, a1                                    # if(1 < k) t5 = 1 else t5 = 0
+    bne     t5, zero, ST_2_k_0                            # if(k > 1) return 0
 
-    j       ST_2_return                                   # Retorna o valor calculado
+    j       ST_2_k_1                                      # Return 1
 
     ST_2_recursion:
-        mv      s1, a0                                    # s1 := a0
-        addi    a0, a0, -1                                # n=n-1
+        addi    sp, sp, -16                               # Aloca espaço na pilha
+        sw      ra, 0(sp)                                 # Aloca endereço de retorno na primeira posição da pilha
+        sw      s5, 4(sp)                                 # Aloca o 'resultado' na segunda posição pilha
+        sw      a0, 8(sp)                                 # Aloca o n na terceira posição da pilha
+        sw      a1, 12(sp)                                # Aloca o k na quarta posição da pilha
+
+        # ST_2(n-1, k)
+        addi    a0, a0, -1                                # n = n-1
         jal     ST_2                                      # recursive call
-        mul     a1, s1, a1                                # a1 = n*n-1
+        mul     s5, a0, a2                                # s5* = n*ST_2(n-1, k)
+        #lw      a0, 8(sp)                                # Recupera o valor de n
+        sw      s5, 4(sp)                                 # Salva o acumulado em s5
 
-        lw      a0, 4(sp)                                 # Restore a0 value from stack
-        sw      a1, 8(sp)                                 # Save accumulate value of return
+        # ST_2(n-1, k-1)
+        lw      a0, 8(sp)                                 # Recupera o valor a da pilha
+        addi    a0, a0, -1                                # a0 = n-1
+        addi    a1, a1, -1                                # a1 = k-1
 
-    ST_2_return:
-        lw      s1, 4(sp)                                 # Load aux register
-        lw      ra, 0(sp)                                 # Return address
-        addi    sp, sp, 12                                # Decrement stack
-        jr      ra                                        # Return to register address position
+        jal     ST_2                                      # "Chamada recursiva"
+        lw      s5, 4(sp)                                 # Recupera o acumulado
+        add     s5, s5, a2                                # s5 = k*ST_2(n-1, k)+ST_2(n-1, k-1)
+        sw      s5, 4(sp)                                 # Devolve o "total" pra pilha
+        add     a2, s5, zero                              # Coloca o acumulado em a2
+
+        lw      ra, 0(sp)
+        addi    sp, sp, 16
+        jr      ra
+    
+    ST_2_k_0:
+        li      a2, 0                                     # Carrega no registrador de retorno o valor zero
+        jr      ra                                        # Retorna pra onde foi chamado
+
+    ST_2_k_1:
+        li      a2, 1                                     # Carrega no registrador de retorno o valor zero
+        jr      ra                                        # Retorna pra onde foi chamado
 # ------------------ Function: until_k ------------------ #
-#                     Arguments: s0                       #
+#                 Arguments: s0(n), s1(k)                 #
 #           Registers in use:             #
 #                      Returns: N/A                       #
 #                     Comments: N/A                       #
 until_k:
-    li      s1, 1                                         #
-    beq     s0, s1, return_until_k                        #
+    li      t1, 1                                         # t1 = 1
+    beq     s1, t1, return_until_k                        # if (k == 1)
 
-    mv      a0, s0                                        #
-    #jal    ST_2                                           #
-    li      a7, 1                                         # 1 é a "Enviroment Call" que "imprime" um "inteiro"
-    ecall                                                 # Realiza ecall
+    add     a0, s0, zero                                  # a0 = n
+    add     a1, s1, zero                                  # a1 = k
+    jal     ST_2                                          #
 
-    la      a0, str1                                      # Carrega o endereço de str0 em a0
+    add     a0, a2, zero
+    li      a7, 1                                     # 1 é a "Enviroment Call" que "imprime" um "inteiro"
+    ecall                                             # Realiza ecall
+
+    la      a0, str1                                      # Carrega o endereço de str1 em a0
     li      a7, 4                                         # 4 é a "Enviroment Call" que faz "impressão" de "string"
     ecall                                                 # Realiza ecall
 
-
-    li      a0, -1                                        # Helper to 64bits
-    add     s0, s0, a0                                    # 
+    addi    s1, s1, -1                                    # k--
     j       until_k                                       # Volta pro until_k
 
     return_until_k:
+        add     a0, s0, zero                              # a0 = n
+        add     a1, s1, zero                              # a1 = k
+        jal     ST_2                                      #
 
-        mv      a0, s0                                    #
-        #jal    ST_2                                       #
+        add     a0, a2, zero
 
         li      a7, 1                                     # 1 é a "Enviroment Call" que "imprime" um "inteiro"
         ecall                                             # Realiza ecall
@@ -97,4 +163,5 @@ until_k:
         li      a7, 4                                     # 4 é a "Enviroment Call" que faz "impressão" de "string"
         ecall                                             # Realiza ecall
 
-        jr      ra                                        # Retorna para registrador origem
+        j        exit
+        #jr      ra                                        # Retorna para registrador origem
